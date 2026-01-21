fb880ea45e2a2e7a5956df556227aae23f535c39
Nike <nikehallberg9@gmail.com>
2026-01-21 13:50:28 +0100

    .

--- src/components/calorieTracker/CalorieTracker.jsx
+++ src/components/calorieTracker/CalorieTracker.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import './CalorieTracker.css'
 
 const CalorieTracker = () => {
--- src/components/mealPlans/MealPlans.jsx
+++ src/components/mealPlans/MealPlans.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import './MealPlans.css'
 
 const MealPlans = () => {
